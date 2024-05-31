09b65553058e463da489fb3084ffa9dde9962cea
deun <devdeun@gmail.com>
2024-05-31 14:08:14 +0900

    사이드 뱃지 추가
    - Lodash로 스크롤 이벤트 throttle 처리
    - GSAP 애니메이션 적용

--- js/main.js
+++ js/main.js
@@ -19,3 +19,21 @@ function handleBlurInput() {
 searchEl.addEventListener("click", handleSearchInputClick);
 searchInputEl.addEventListener("focus", handleFocusInput);
 searchInputEl.addEventListener("blur", handleBlurInput);
+
+const badgeEl = document.querySelector(".side-badges");
+
+function handleWindowScroll() {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: "none",
+    });
+  } else {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: "block",
+    });
+  }
+}
+
+window.addEventListener("scroll", _.throttle(handleWindowScroll, 300));
