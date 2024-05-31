4714ffdd743f7c554934cc7da5bc48761da9fabf
deun <devdeun@gmail.com>
2024-05-31 12:16:45 +0900

    헤더 및 드롭다운 메뉴 추가

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector(".search-input");
+const searchIconEl = searchEl.querySelector(".search-icon");
+
+function handleSearchInputClick() {
+  searchInputEl.focus();
+}
+
+function handleFocusInput() {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+}
+
+function handleBlurInput() {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+}
+
+searchEl.addEventListener("click", handleSearchInputClick);
+searchInputEl.addEventListener("focus", handleFocusInput);
+searchInputEl.addEventListener("blur", handleBlurInput);
