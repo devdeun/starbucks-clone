6a8a9c19ff8f6fdbba2cd6f6fdd4e906cf636072
deun <devdeun@gmail.com>
2024-06-01 20:31:25 +0900

    프로모션 추가
    - swiper를 이용한 슬라이드 구현
    - toggle 버튼으로 열고 닫을 수 있음

--- js/main.js
+++ js/main.js
@@ -51,3 +51,38 @@ new Swiper(".swiper-notice", {
   autoplay: true,
   loop: true,
 });
+new Swiper(".swiper-promotion", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000, // 5s
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+let isHidePromotion = true;
+
+function handlePromotionToggleBtnClick() {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add("hide");
+    promotionToggleBtn.classList.add("rotate");
+  } else {
+    promotionEl.classList.remove("hide");
+    promotionToggleBtn.classList.remove("rotate");
+  }
+}
+
+promotionToggleBtn.addEventListener("click", handlePromotionToggleBtnClick);
